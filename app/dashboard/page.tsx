b41497872ae775aa42b2d53daf33521aaa923540
mustafa-bugra-yildiz <mustafa.bugra.yildiz@icloud.com>
2025-02-01 11:01:33 +0300

    Add sign out button to dashboard banner

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { databasesTable } from "@/db/schema";
 import { currentUser, User } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import DatabaseList from "./database-list";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default async function Dashboard() {
 	const user = (await currentUser())!
@@ -26,6 +27,7 @@ function Banner({ user }: { user: User }) {
 			<h1 className="text-4xl font-bold">AIyssa</h1>
 			<p className="text-lg text-gray-500">Your Personal Data Assistant</p>
 			<p>{user?.fullName}</p>
+			<SignOutButton />
 		</div>
 	)
 }
