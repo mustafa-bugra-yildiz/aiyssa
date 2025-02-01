1d8b6e77ab7255116744739c4244b54159c889d8
mustafa-bugra-yildiz <mustafa.bugra.yildiz@icloud.com>
2025-02-01 09:53:01 +0300

    Display current user's full name on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">AIyssa</h1>
-      <p className="text-lg text-gray-500">Your Personal Data Assistant</p>
-    </div>
-  );
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+	const user = await currentUser()
+
+	return (
+		<div className="flex flex-col items-center justify-center h-screen gap-4">
+			<h1 className="text-4xl font-bold">AIyssa</h1>
+			<p className="text-lg text-gray-500">Your Personal Data Assistant</p>
+			<p>{user?.fullName}</p>
+		</div>
+	);
 }
